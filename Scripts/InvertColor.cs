c12cbc3c4aabccc52f238f4f0055c76febdc9363
hungmingjyun <hungmingjyun@team-lab.com>
2022-06-02 19:57:47 +0800

    invert color

---
+++ Scripts/InvertColor.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using System;
+
+[Serializable, VolumeComponentMenu("Post-processing/Custom/InvertColor")]
+public sealed class InvertColor : CustomPostProcessVolumeComponent, IPostProcessComponent
+{
+    public BoolParameter revert = new BoolParameter(false);
+
+    Material m_Material;
+
+    public bool IsActive() => m_Material != null;
+
+    // Do not forget to add this post process in the Custom Post Process Orders list (Project Settings > Graphics > HDRP Settings).
+    public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
+
+    const string kShaderName = "Hidden/Shader/InvertColor";
+
+    public override void Setup()
+    {
+        if (Shader.Find(kShaderName) != null)
+            m_Material = new Material(Shader.Find(kShaderName));
+        else
+            Debug.LogError($"Unable to find shader '{kShaderName}'. Post Process Volume InvertColor is unable to load.");
+    }
+
+    public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
+    {
+        if (m_Material == null)
+            return;
+
+        m_Material.SetFloat("_Revert", revert.value ? 1 : 0);
+        cmd.Blit(source, destination, m_Material, 0);
+    }
+
+    public override void Cleanup()
+    {
+        CoreUtils.Destroy(m_Material);
+    }
+}
