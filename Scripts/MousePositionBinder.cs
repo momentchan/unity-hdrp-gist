4e9ba8defb4a2c35bac924f87ceaffaffe5252a6
hungmingjyun <hungmingjyun@team-lab.com>
2022-08-02 12:01:32 +0900

    mouse view pos

--- Scripts/MousePositionBinder.cs
+++ Scripts/MousePositionBinder.cs
@@ -7,17 +7,25 @@ namespace mj.gist {
     public class MousePositionBinder : VFXBinderBase {
         [VFXPropertyBinding("UnityEngine.Vector3")]
         public ExposedProperty mouseWorldProperty;
+        [VFXPropertyBinding("UnityEngine.Vector3")]
+        public ExposedProperty mouseViewportProperty;
         [SerializeField] private float depth = 0;
 
         public override bool IsValid(VisualEffect component) {
-            return component.HasVector3(mouseWorldProperty);
+            return component.HasVector3(mouseWorldProperty) || component.HasVector3(mouseViewportProperty);
         }
 
         public override void UpdateBinding(VisualEffect component) {
             var mouseScreen = Input.mousePosition;
             mouseScreen.z = depth;
+
             var worldPosition = Camera.main.ScreenToWorldPoint(mouseScreen);
-            component.SetVector3(mouseWorldProperty, worldPosition);
+            var viewPosition = Camera.main.ScreenToViewportPoint(mouseScreen);
+
+            if (component.HasVector3(mouseWorldProperty))
+                component.SetVector3(mouseWorldProperty, worldPosition);
+            if (component.HasVector3(mouseViewportProperty))
+                component.SetVector3(mouseViewportProperty, viewPosition);
         }
     }
 }
